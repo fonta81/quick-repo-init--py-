a1ccd78e66bb43cf3a881dbfaf4b126e97db8da4
mteo <cortinamateo211@gmail.com>
2026-07-01 20:47:11 -0500

    Base.py/Cualquier .py/.js/.sh/ect
    -ahora el programa puede crear cual extencion que le de el usuario

--- Base.py
+++ Base.py
@@ -13,20 +13,11 @@ class BasemodClass:
         except Exception as e:
             print(f"Error: {e}")
 
-    def CrearPy(self, NombrePy):
+    def CrearArchivo(self, Nombre, Extencion):
         try:
-            ExtencionArchivoPy = NombrePy + ".py"
-            PyCrear = os.path.join(self.path, ExtencionArchivoPy)
-            with open(PyCrear, "w", encoding="utf=8"):
-                pass
-        except Exception as e:
-            print(f"Error: {e}")
-
-    def CrearBash(self, NombreBash):
-        try:
-            ExtencionArchivoBash = NombreBash + ".sh"
-            BashCrear = os.path.join(self.path, ExtencionArchivoBash)
-            with open(BashCrear, "w", encoding="utf-8"):
+            ExtencionArchivo = Nombre + Extencion
+            Crear = os.path.join(self.path, ExtencionArchivo)
+            with open(Crear, "w", encoding="utf=8"):
                 pass
         except Exception as e:
             print(f"Error: {e}")
@@ -49,13 +40,10 @@ class BasemodClass:
         print("\n")
         self.CrearCarpeta(input("Digite el nombre de la carpeta a crear: "))
         print("\n")
-        elecc = input("Digite que archivo quiere crear:\n1.python \n2.bash\nElige: ")
-        if elecc == "1":
-            self.CrearPy(input("Digite el nombre del archivo python a crear: "))
-        elif elecc == "2":
-            self.CrearBash(input("Digite el nombre del archivo Bash a crear: "))
-        else:
-            print("Ingrese un valor valido ")
+        self.CrearArchivo(
+            input("Ecriba el nombre del archivo a crear: "),
+            input("Escriba la extencion del archivo a crear .py/.sh/.js/.html/ect: "),
+        )
         self.CrearRead()
         self.CrearRepo()
         print("\n")
