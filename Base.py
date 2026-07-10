d555f7a918b8c3514ccd3fe090a4c20a7202135c
mteo <cortinamateo211@gmail.com>
2026-07-10 09:54:02 -0500

    Comentarios

--- Base.py
+++ Base.py
@@ -4,49 +4,49 @@ import subprocess
 
 class BasemodClass:
     def __init__(self):
-        self.path = ""
+        self.path = ""  # Crea variable vacia
 
     def CrearCarpeta(self, NombreCc):
         try:
-            os.makedirs(NombreCc, exist_ok=True)
-            self.path = os.path.join("", NombreCc)
-        except Exception as e:
+            os.makedirs(NombreCc, exist_ok=True)  # crea carpeta
+            self.path = os.path.join("", NombreCc)  # asigna path -> variable
+        except Exception as e:  # en caso de error:
             print(f"Error: {e}")
 
     def CrearArchivo(self, Nombre, Extencion):
         try:
-            ExtencionArchivo = Nombre + Extencion
-            Crear = os.path.join(self.path, ExtencionArchivo)
-            with open(Crear, "w", encoding="utf=8"):
+            ExtencionArchivo = Nombre + Extencion  # agrega extencion (.py,ect)
+            Crear = os.path.join(self.path, ExtencionArchivo)  # crea path del archivo
+            with open(Crear, "w", encoding="utf=8"):  # crea archivo
                 pass
-        except Exception as e:
+        except Exception as e:  # en caso de error:
             print(f"Error: {e}")
 
     def CrearRead(self):
         try:
-            ReadCrear = os.path.join(self.path, "Readme.md")
-            with open(ReadCrear, "w", encoding="utf-8"):
+            ReadCrear = os.path.join(self.path, "Readme.md")  # crea path readme
+            with open(ReadCrear, "w", encoding="utf-8"):  # crea el readme
                 pass
-        except Exception as e:
+        except Exception as e:  # en caso de error:
             print(f"Error: {e}")
 
     def CrearRepo(self):
-        try:
+        try:  # crea repo en carpeta creada:
             subprocess.run(["git", "-C", self.path, "init"], capture_output=True)
-        except Exception as e:
+        except Exception as e:  # en caso de error:
             print(f"Error: {e}")
 
     def Usuario(self):
-        print("\n")
+        print("\n")  # Se le pide nombre de carpeta:
         self.CrearCarpeta(input("Digite el nombre de la carpeta a crear: "))
         print("\n")
-        self.CrearArchivo(
+        self.CrearArchivo(  # se le pide el nombre del archivo y su extencion:
             input("Ecriba el nombre del archivo a crear: "),
             input("Escriba la extencion del archivo a crear .py/.sh/.js/.html/ect: "),
         )
-        self.CrearRead()
-        self.CrearRepo()
-        print("\n")
+        self.CrearRead()  # crea readme
+        self.CrearRepo()  # crea repo
+        print("\n")  # mensaje de exito:
         print("El Proceso se ejecuto correctamente")
         return
 
@@ -69,5 +69,5 @@ def main():  # El menu principal
             print("Ingrese un valor válido")
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # inicia el main
     main()
